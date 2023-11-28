7a2da4bf8f7809ad325aabaf89714f870481489b
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-11-28 09:02:31 +0530

    Add files via upload

--- Pattern1.java
+++ Pattern1.java
@@ -1,33 +1,17 @@
 import java.util.*;
 class Pattern1
 {
-	public static void main(String agrs[])
+	public static void main(String args[])
 	{
-		int i,j,k,n;
+		int i,j,n;
 		Scanner s=new Scanner(System.in);
 		System.out.println("enter the no.of rows: ");
 		n=s.nextInt();
 		for(i=1;i<=n;i++)
 		{
-			for(j=n-i;j>=1;j--)
+			for(j=1;j<=i;j++)
 			{
-				System.out.print(" ");
-			}
-			for(k=1;k<=i;k++)
-			{
-				System.out.print(i+" ");
-			}
-			System.out.println();
-		}
-		for(i=n-1;i>=0;i--)
-		{
-			for(j=n-i;j>=1;j--)
-			{
-				System.out.print(" ");
-			}
-			for(k=1;k<=i;k++)
-			{
-				System.out.print(i+" ");
+				System.out.print("*");
 			}
 			System.out.println();
 		}
